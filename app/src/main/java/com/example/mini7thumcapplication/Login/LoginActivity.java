0b89365a50c0e892fec70af11016ff01083fb474
user <sally09290@duksung.ac.kr>
2024-12-03 00:55:36 +0900

    xml 전체 수정 완료

--- app/src/main/java/com/example/mini7thumcapplication/Login/LoginActivity.java
+++ app/src/main/java/com/example/mini7thumcapplication/Login/LoginActivity.java
@@ -2,8 +2,11 @@ package com.example.mini7thumcapplication.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mini7thumcapplication.MainActivity;
 import com.example.mini7thumcapplication.R;
 
 
@@ -18,6 +21,18 @@ public class LoginActivity extends AppCompatActivity {
         joinButton.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
             startActivity(intent);
+        });
+
+        // 로그인 버튼 연결
+        Button loginButton = findViewById(R.id.loginButton); // 로그인 버튼 ID 확인
+        loginButton.setOnClickListener(v -> {
+            // 로그인 성공 후 MainActivity로 이동
+            Log.d("login Activity","로그인 성공");
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            Log.d("login Activity","intent");
+            startActivity(intent);
+            Log.d("login Activity","액티비티 연결");
+            finish(); // LoginActivity 종료
         });
     }
 }
